09c364337c2e8703150532714e1db5d866f0cedc
Daniele Trifirò <daniele@iterative.ai>
2022-06-21 23:21:35 +0200

    add aws_region_name fixture
    When not providing `region_name` to the s3 client, the moto server returns
    400 and aiobotocore raises: "IllegalLocationConstraintException when calling
    the CreateBucket operation: The unspecified location constraint is incompatible
    for the region specific endpoint this request was sent to."

--- src/pytest_servers/fixtures.py
+++ src/pytest_servers/fixtures.py
@@ -8,6 +8,11 @@ from .s3 import MockedS3Server
 
 
 @pytest.fixture
+def aws_region_name():
+    return "eu-west-1"
+
+
+@pytest.fixture
 def s3_server(monkeypatch):
     monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "foo")
     monkeypatch.setenv("AWS_ACCESS_KEY_ID", "foo")
@@ -16,10 +21,14 @@ def s3_server(monkeypatch):
 
 
 @pytest.fixture
-def s3path(s3_server):
+def s3path(s3_server, aws_region_name):
     path = UPath(
         "s3://test",
-        client_kwargs={"endpoint_url": s3_server.endpoint_url},
+        client_kwargs={
+            "endpoint_url": s3_server.endpoint_url,
+            # NOTE: When not providing a region, moto returns 400
+            "region_name": aws_region_name,
+        },
     )
     path.mkdir()
     yield path
