9abc21e502039cea97990c77c773777a41524d4d
Daniele Trifirò <daniele@iterative.ai>
2022-06-23 11:17:51 +0200

    add fixtures docstrings

--- src/pytest_servers/fixtures.py
+++ src/pytest_servers/fixtures.py
@@ -14,6 +14,7 @@ def aws_region_name():
 
 @pytest.fixture
 def s3_server(monkeypatch):
+    """Spins up a moto s3 server on 127.0.0.1:5555."""
     monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "foo")
     monkeypatch.setenv("AWS_ACCESS_KEY_ID", "foo")
     with MockedS3Server() as server:
@@ -22,6 +23,7 @@ def s3_server(monkeypatch):
 
 @pytest.fixture
 def s3path(s3_server, aws_region_name):
+    """Return a temporary path on a mocked S3 remote."""
     path = UPath(
         "s3://test",
         client_kwargs={
@@ -36,6 +38,7 @@ def s3path(s3_server, aws_region_name):
 
 @pytest.fixture
 def local_path(tmp_path_factory, monkeypatch):
+    """Return a temporary path."""
     ret = LocalPath(tmp_path_factory.mktemp("pytest-servers"))
     monkeypatch.chdir(ret)
     yield ret
@@ -43,6 +46,13 @@ def local_path(tmp_path_factory, monkeypatch):
 
 @pytest.fixture
 def tmp_upath(request):
+    """Return a temporary directory on several filesystems.
+
+    Usage:
+    >>> @pytest.mark.parametrize("tmp_upath", ["local", "s3"], indirect=True])
+    >>> def test_something(tmp_upath):
+    >>>     pass
+    """
     param = getattr(request, "param", "local")
     if param == "local":
         return request.getfixturevalue("local_path")
