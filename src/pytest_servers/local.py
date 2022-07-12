578a0fc637bd9286bdee279134bd932ea8bca9fe
Daniele Trifirò <daniele@iterative.ai>
2022-07-12 15:15:23 +0200

    local: add path attribute to LocalFileSystem

--- src/pytest_servers/local.py
+++ src/pytest_servers/local.py
@@ -5,3 +5,7 @@ from fsspec.implementations.local import LocalFileSystem
 
 class LocalPath(type(pathlib.Path())):  # type: ignore[misc]
     fs = LocalFileSystem()
+
+    @property
+    def path(self):
+        return str(self)
