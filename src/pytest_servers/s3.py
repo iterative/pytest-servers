711c3c5a4d75622a3c2aa6e400d5ab61ae50f20d
Daniele Trifirò <daniele@iterative.ai>
2022-07-25 18:17:54 +0200

    utils: add timeout to wait_until

--- src/pytest_servers/s3.py
+++ src/pytest_servers/s3.py
@@ -43,7 +43,9 @@ class MockedS3Server:
         )
         out = self.proc.stderr.readline()
         self.port = int(re.match(b".*http://127.0.0.1:(\\d+).*", out).group(1))
-        wait_until(silent(lambda: requests.get(self.endpoint_url).ok), 5)
+        wait_until(
+            silent(lambda: requests.get(self.endpoint_url, timeout=5).ok), 5
+        )
 
         return self
 
