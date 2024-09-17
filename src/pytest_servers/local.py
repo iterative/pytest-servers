23d2f1fd5e1e3e31859d2ae2f5aa2b3c1337ae4d
Matt Seddon <mattseddon@hotmail.com>
2024-09-18 08:43:14 +1000

    ignore lint issue

--- src/pytest_servers/local.py
+++ src/pytest_servers/local.py
@@ -7,5 +7,5 @@ class LocalPath(type(pathlib.Path())):  # type: ignore[misc]
     fs = LocalFileSystem()
 
     @property
-    def path(self) -> str:
+    def path(self) -> str:  # noqa: N804
         return str(self)
